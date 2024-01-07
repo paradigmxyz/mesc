3f9fed2214e7a4969fa6e7dbd9fc251f4fb635e6
sslivkoff <stormslivkoff@gmail.com>
2024-01-07 00:58:20 -0800

    use single quotes format with ruff

--- python/mesc/interface.py
+++ python/mesc/interface.py
@@ -12,10 +12,10 @@ def is_mesc_enabled() -> bool:
     1) MESC_MODE != "DISABLED"
     2) at least 1 MESC env var is set
     """
-    if os.environ.get("MESC_MODE") == "DISABLED":
+    if os.environ.get('MESC_MODE') == 'DISABLED':
         return False
     for var in mesc_env_vars:
-        if os.environ.get(var) not in [None, ""]:
+        if os.environ.get(var) not in [None, '']:
             return True
     return False
 
@@ -28,12 +28,12 @@ def get_default_endpoint(
         config = load.read_config_data()
 
     # get endpoint name
-    if profile is not None and profile in config["profiles"]:
-        endpoint = config["profiles"][profile].get(
-            "default_endpoint", config["default_endpoint"]
+    if profile is not None and profile in config['profiles']:
+        endpoint = config['profiles'][profile].get(
+            'default_endpoint', config['default_endpoint']
         )
     else:
-        endpoint = config["default_endpoint"]
+        endpoint = config['default_endpoint']
 
     # get endpoint
     if endpoint is None:
@@ -47,9 +47,9 @@ def get_endpoint_by_name(name: str, *, config: RpcConfig | None = None) -> Endpo
     if config is None:
         config = load.read_config_data()
     try:
-        return config["endpoints"][name]
+        return config['endpoints'][name]
     except KeyError:
-        raise exceptions.MissingEndpoint("missing endpoint: " + str(name))
+        raise exceptions.MissingEndpoint('missing endpoint: ' + str(name))
 
 
 def get_endpoint_by_network(
@@ -64,14 +64,14 @@ def get_endpoint_by_network(
 
     # get global default for network
     if chain_id is None:
-        raise ValueError("chain_id must be a str")
+        raise ValueError('chain_id must be a str')
     chain_id = str(chain_id)
-    network_defaults = config["network_defaults"]
+    network_defaults = config['network_defaults']
     default_name = network_defaults.get(chain_id)
 
     # get profile default for network
-    if profile and profile in config["profiles"]:
-        name = config["profiles"][profile]["network_defaults"].get(
+    if profile and profile in config['profiles']:
+        name = config['profiles'][profile]['network_defaults'].get(
             chain_id, default_name
         )
     else:
@@ -99,8 +99,8 @@ def get_endpoint_by_query(
     """
     if config is None:
         config = load.read_config_data()
-    if user_input in config["endpoints"]:
-        return config["endpoints"][user_input]
+    if user_input in config['endpoints']:
+        return config['endpoints'][user_input]
     elif user_input.isdecimal():
         try:
             get_endpoint_by_network(int(user_input), profile=profile)
@@ -131,26 +131,26 @@ def find_endpoints(
 
     if config is None:
         config = load.read_config_data()
-    endpoints = list(config["endpoints"].values())
+    endpoints = list(config['endpoints'].values())
 
     # check chain_id
     if chain_id is not None:
         if isinstance(chain_id, int):
             chain_id = str(chain_id)
         endpoints = [
-            endpoint for endpoint in endpoints if endpoint["chain_id"] == chain_id
+            endpoint for endpoint in endpoints if endpoint['chain_id'] == chain_id
         ]
 
     # check name_contains
     if name_contains is not None:
         endpoints = [
-            endpoint for endpoint in endpoints if name_contains in endpoint["name"]
+            endpoint for endpoint in endpoints if name_contains in endpoint['name']
         ]
 
     # check url_contains
     if url_contains is not None:
         endpoints = [
-            endpoint for endpoint in endpoints if url_contains in endpoint["url"]
+            endpoint for endpoint in endpoints if url_contains in endpoint['url']
         ]
 
     return endpoints
@@ -158,4 +158,4 @@ def find_endpoints(
 
 def get_global_metadata() -> Mapping[str, Any]:
     """return MESC global metadata"""
-    return load.read_config_data()["global_metadata"]
+    return load.read_config_data()['global_metadata']
