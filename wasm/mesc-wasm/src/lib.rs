858ec0cc507ed84b57ae0d51789b092c19296e2f
omar <omaraziz.dev@proton.me>
2024-02-25 14:34:48 -0800

    feat: start integrating mesc fns

--- wasm/mesc-wasm/src/lib.rs
+++ wasm/mesc-wasm/src/lib.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+pub use mesc::*;
+use serde_wasm_bindgen::to_value;
 use wasm_bindgen::prelude::*;
 
 #[cfg(feature = "wee_alloc")]
@@ -8,10 +10,19 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
 extern "C" {
-    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello,{}!", name));
+pub fn is_mesc_enabled_wrapper() -> bool {
+    mesc::is_mesc_enabled()
+}
+
+#[wasm_bindgen]
+pub fn get_default_endpoint_wrapper(option: Option<String>) -> Result<JsValue, JsValue> {
+    let option = option.as_deref();
+    let result =
+        mesc::get_default_endpoint(option).map_err(|e| JsValue::from_str(&e.to_string()))?;
+    to_value(&result).map_err(|e| JsValue::from_str(&e.to_string()))
 }
--- wasm/src/index.ts
+++ wasm/src/index.ts
@@ -1,8 +1,12 @@
-import init, { greet } from 'mesc_wasm/mesc_wasm.js'
+import init, { get_default_endpoint_wrapper, is_mesc_enabled_wrapper } from 'mesc_wasm/mesc_wasm.js'
+
+const mescEnabledElement = document.querySelector('span#mesc-enabled')
 
 async function run() {
   await init()
-  greet('mesc')
+  const isMescEnabled = is_mesc_enabled_wrapper()
+  console.log(`is mesc enabled: ${isMescEnabled}`)
+  if (mescEnabledElement) mescEnabledElement.textContent = isMescEnabled.toString()
 }
 
 run()
