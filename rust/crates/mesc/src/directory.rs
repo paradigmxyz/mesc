c64c47f4c2c9d2b7354b9983ef7db20097510706
sslivkoff <stormslivkoff@gmail.com>
2024-08-13 12:58:48 -0700

    use full list of network names in network query

--- rust/crates/mesc/src/directory.rs
+++ rust/crates/mesc/src/directory.rs
@@ -15,15 +15,21 @@ fn known_networks() -> HashMap<&'static str, ChainId> {
 }
 
 /// get chain id of given network name
-pub(crate) fn get_network_chain_id(network: &str) -> Option<ChainId> {
-    known_networks().remove(network)
+pub(crate) fn get_network_chain_id(network_name: &str) -> Option<ChainId> {
+    let network_names = crate::network_names::get_network_names();
+    for (chain_id, network) in network_names.into_iter() {
+        if network == network_name {
+            return Some(chain_id);
+        }
+    }
+    None
 }
 
 /// get network name of given chain id
 pub fn get_network_name(chain_id: &ChainId) -> Option<String> {
     for (name, other_chain_id) in known_networks().iter() {
         if chain_id == other_chain_id {
-            return Some(name.to_string())
+            return Some(name.to_string());
         }
     }
     None
